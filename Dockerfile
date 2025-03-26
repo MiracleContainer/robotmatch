c4d5388a17530fb6fa8f57e7e182ef33602cd207
Eric Wei <30461354+MiracleContainer@users.noreply.github.com>
2025-03-26 14:17:25 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# 构建阶段
+FROM node:18-alpine AS builder
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci
+COPY . .
+RUN npm run build
+
+# 运行阶段
+FROM node:18-alpine
+WORKDIR /app
+ENV NODE_ENV production
+COPY --from=builder /app/package.json ./package.json
+COPY --from=builder /app/next.config.js ./
+COPY --from=builder /app/public ./public
+COPY --from=builder /app/.next/standalone ./
+COPY --from=builder /app/.next/static ./.next/static
+
+EXPOSE 3000
+CMD ["npm", "start"]
---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+      { protocol: 'https', hostname: 'pexels.com' },
+      { protocol: 'https', hostname: 'via.placeholder.com' }
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
